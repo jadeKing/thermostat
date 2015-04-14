8c0057f8182271642e2b0f2c34bad916684c7615
Ilya Faybisovich <Ilya.Faybisovich@gmail.com>
2015-04-14 17:24:59 +0300

    Increasing/decreasing temperature by 1

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,9 +1,20 @@
 describe ('Thermostat',function(){
   var thermostat;
+  
   beforeEach(function(){
     thermostat = new Thermostat();
   });
+  
   it ('shows 20 degrees as default', function(){
     expect(thermostat.showTemperature()).toEqual(20);
+  });
+
+
+  it ('increases temperature by 1 degree', function(){
+  	expect(thermostat.increaseTemperatureBy1()).toEqual(21);
+  });
+
+  it ('decreases temperature by 1 degree', function() {
+  	expect(thermostat.decreaseTemperatureBy1()).toEqual(19);
   });
 });
